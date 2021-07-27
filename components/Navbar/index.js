fff23d09e3e848e198656a420171f5ca0aae8723
meir-gazit <emgazit@gmail.com>
2021-07-27 12:41:18 +0300

    first playground commit

---
+++ components/Navbar/index.js
@@ -0,0 +1,23 @@
+export default function Navbar() {
+  return (
+    <div name="wrapper" className="grid grid-cols-12 gap-2">
+      <div name="row-1-99" className="border">1</div>
+      <div name="row-1" className="border">2</div>
+      <div name="row-1" className="border">3</div>
+      <div name="row-1" className="border col-span-8">4</div>
+      <div name="row-1" className="border">12</div>
+    
+      <div name="row-2" className="border">1</div>
+      <div name="row-2" className="border">2</div>
+      <div name="row-2" className="border">3</div>
+      <div name="row-2" className="border">4</div>
+      <div name="row-2" className="border">5</div>
+      <div name="row-2" className="border">6</div>
+      <div name="row-2" className="border">7</div>
+      <div name="row-2" className="border">8</div>
+      <div name="row-2" className="border">9</div>
+      <div name="row-2" className="border">10</div>
+      <div name="row-2" className="border">11</div>
+      <div name="row-2" className="border">12</div>
+  </div>)
+}
